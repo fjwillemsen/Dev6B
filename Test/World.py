ef40f8d2da34bb96661b03f250b3e55c44cf5ce3
mindecheng <mindecheng@live.nl>
2016-11-29 22:42:06 +0100

    shz this szzz

---
+++ Test/World.py
@@ -0,0 +1,10 @@
+import unittest
+
+class Test_World(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
