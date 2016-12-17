122d671e2f81418cdd16a113036ac57c44783985
0912293 <Kevin181094@gmail.com>
2016-12-17 20:52:47 +0100

    Finished spar game now with timer and different types of input (still need to generate questions+answer pairs from database)

--- Main.py
+++ Main.py
@@ -42,17 +42,31 @@ def spar():
 
 @app.route('/spar-game', methods=['POST'])
 def sparg():
+    spar_game.gen_question(spar_game)
+    spar_game.gen_answers(spar_game)
     item = request.form['item']
+    spar_game.setitem = item
+    spar_game.difficulty(spar_game,item)
     print(item)
-    return spar_game.get(spar_game, item,spar_game.generate_answers(spar_game),spar_game.generate_answers(spar_game),spar_game.generate_answers(spar_game),spar_game.generate_answers(spar_game))
+    return spar_game.get(spar_game, item,spar_game.get_quest(spar_game,0),spar_game.get_quest(spar_game,1),
+                         spar_game.get_quest(spar_game,2),spar_game.get_quest(spar_game,3),
+                         spar_game.get_quest(spar_game,4),spar_game.get_answer(spar_game,0,0),
+                         spar_game.get_answer(spar_game,0,1),spar_game.get_answer(spar_game,0,2),
+                         spar_game.get_answer(spar_game,1,0),spar_game.get_answer(spar_game,2,0),
+                         spar_game.get_answer(spar_game,2,1),spar_game.get_answer(spar_game,2,2),
+                         spar_game.get_answer(spar_game,3,0),spar_game.get_answer(spar_game,3,1),
+                         spar_game.get_answer(spar_game,3,2),spar_game.get_answer(spar_game,4,0),
+                         spar_game.get_answer(spar_game,4,1),spar_game.get_answer(spar_game,4,2),
+                         spar_game.getscore(spar_game))
 
 @app.route('/spar-check', methods=['POST'])
 def sparcheck():
-    useranswer = request.form['answer']
-    answer = spar_game.generate_answers(spar_game)
-    print(answer)
-    return spar_game.check(spar_game, useranswer, answer)
-
+    useranswer = [request.form['q1a'],request.form['q2a'],request.form['q3a'],request.form['q4a'],request.form['q5a']]
+    for index in range(len(useranswer)):
+        print("Correct answer:" + str(spar_game.get_correctA(spar_game, index) + ". Input: " + useranswer[index]))
+        if spar_game.get_correctA(spar_game,index) == useranswer[index]:
+            spar_game.incscore(spar_game,50)
+    return spar_game.check(spar_game, spar_game.getscore(spar_game))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- spar_game.py
+++ spar_game.py
@@ -3,30 +3,102 @@ from string import Template
 import random
 
 class spar_game:
-    count=0
+    count = 0
+    score = 0
+
+    time = 0
+
+    item = ''
+
+    info="Enter information that helps with answering the questions"
+    correctA=['','','','','']
+    q=['','','','','']
+    q1a=['','','']
+    q2a=['']
+    q3a=['','','']
+    q4a=['','','']
+    q5a=['','','']
+
     def __init__(self, duration):
         self.sDuration = duration
 
-    def generate_question(self):
+    def gen_question(self):
         #get question from database
-        question = "test"
-        return question
+        for index in range(len(self.q)):
+            self.q[index] = "test"+str(index)
+            print(self.q[index])
 
-    def generate_answers(self):
-        #get answers from database
-        if(self.count<2):
-            self.count+=1
-            answer = "test1"
-        else:
-            answer = "test2"
-        return answer
+    def get_quest(self,i):
+        return self.q[i]
 
-    def get(self,sitem,quest,ans1,ans2,ans3):
-        template = Template(render_template("spar_game.html"))
-        return template.substitute(item=sitem,question=quest,answer1=ans1,answer2=ans2,answer3=ans3,discount=25)
+    def gen_answers(self):
+        # get answers from database
+        for index in range(len(self.q1a)):
+            self.correctA[0] = "test"+str(index+1)
+            self.q1a[index] = "test"+str(index+1)
+            print(self.q1a[index])
+        for index in range(len(self.q2a)):
+            self.correctA[1] = "test" + str(index + 1)
+            self.q2a[index] = "test"+str(index+1)
+            print(self.q2a[index])
+        for index in range(len(self.q3a)):
+            self.correctA[2] = "test" + str(index + 1)
+            self.q3a[index] = "test"+str(index+1)
+            print(self.q3a[index])
+        for index in range(len(self.q4a)):
+            self.correctA[3] = "test" + str(index + 1)
+            self.q4a[index] = "test"+str(index+1)
+            print(self.q4a[index])
+        for index in range(len(self.q5a)):
+            self.correctA[4] = "test" + str(index + 1)
+            self.q5a[index] = "test"+str(index+1)
+            print(self.q5a[index])
 
-    def check(self, user_answer, answer):
-        if user_answer == answer:
-            return "correct"
-        elif user_answer != answer:
-            return "incorrect"
+    def get_correctA(self,u):
+        if (u == 0):
+            return self.correctA[u]
+        elif (u == 1):
+            return self.correctA[u]
+        elif (u == 2):
+            return self.correctA[u]
+        elif (u == 3):
+            return self.correctA[u]
+        elif (u == 4):
+            return self.correctA[u]
+
+
+    def get_answer(self,i,u):
+        if(i==0):
+            return self.q1a[u]
+        elif (i == 1):
+            return self.q2a[u]
+        elif (i == 2):
+            return self.q3a[u]
+        elif (i == 3):
+            return self.q4a[u]
+        elif (i == 4):
+            return self.q5a[u]
+
+    def difficulty(self,item):
+        if item == 'energy':
+            self.time = 3
+        elif item == 'bread':
+            self.time = 2
+        elif item == 'pizza':
+            self.time = 1
+
+    def incscore(self,i):
+        self.score += i
+
+    def getscore(self):
+        return self.score
+
+    def get(self,it,q1,q2,q3,q4,q5,q1a1,q1a2,q1a3,q2a1,q3a1,q3a2,q3a3,q4a1,q4a2,q4a3,q5a1,q5a2,q5a3,s):
+        template = Template(render_template("spar_game.html",radio=True,open=False,info=False))
+        return template.substitute(item=it,q1=q1,q2=q2,q3=q3,q4=q4,q5=q5,q1a1=q1a1,q1a2=q1a2,q1a3=q1a3,q2a1=q2a1,q3a1=q3a1,
+                                   q3a2=q3a2,q3a3=q3a3,q4a1=q4a1,q4a2=q4a2,q4a3=q4a3,q5a1=q5a1,q5a2=q5a2,q5a3=q5a3,score=s,t=self.time)
+
+    def check(self, s):
+        template = Template(render_template("spar_res.html"))
+        self.score = 0
+        return template.substitute(score=s)
