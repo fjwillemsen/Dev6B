5ca02ac2645687892f87046312005663296987a7
0912293 <Kevin181094@gmail.com>
2016-12-21 12:56:01 +0100

    fixed shit.... I hope

--- Spar.py
+++ Spar.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template
 
 class Spar:
-    def __init__(self, duration):
-        self.sDuration = duration
 
     def get(self):
         return render_template("spar.html")
