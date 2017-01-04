c6b28107c580bf73dc93d7e5f83abcdc230abea7
mindecheng <mindecheng@live.nl>
2017-01-04 23:21:08 +0100

    1 project tegelijke tijd, added process, added name

--- ProjectTask.py
+++ ProjectTask.py
@@ -7,15 +7,31 @@ class ProjectTask:
         self.project4 = [("4-1",'task1',5),("4-2",'task2',10),("4-3",'task3',10),("4-4",'task4',10),("4-5",'task5',10),("4-6",'task6',10)];
         self.project56 = [("56-1",'task1',5),("56-2",'task2',10),("56-3",'task3',10),("56-4",'task4',10),("56-5",'task5',10),("56-6",'task6',10)];
         self.project78 = [("78-1",'task1',5),("78-2",'task2',10),("78-3",'task3',10),("78-4",'task4',10),("78-5",'task5',10),("78-6",'task6',10)];
+        self.listofProjectNames =["project 1","project 2","project 3","project 4","project 56","project 78"];
         self.listOfProjectTask = (self.project1,self.project2,self.project3,self.project4,self.project56,self.project78);
         self.lastActionDate = None;
         self.cooldownSecOver = None;
+        self.listItemsPerProject = [len(self.project1),len(self.project2),len(self.project3),len(self.project4),len(self.project56),len(self.project78)];
 
     def getListOfTask(self,project_id):
         self.project_id =project_id-1
         return self.listOfProjectTask[self.project_id]
 
+    def getItemDoneForProject(self,project_id):
+        nr= str(0+ self.listItemsPerProject[project_id-1] - len(self.listOfProjectTask[project_id-1]) )+"/" +str(self.listItemsPerProject[project_id-1])
+        return nr
+    def getProjectNumber(self):
+        x = 0
+        y = 0
+        while(x==0):
+            if(len(self.listOfProjectTask[y]) != 0):
+                x = y +1
+            else:
+                y = y +1
 
+        return x
+    def getProjectName(self,project_id):
+        return self.listofProjectNames[project_id-1]
 
     def updateListOftask(self,task_id):
         for projectTask in self.listOfProjectTask:
