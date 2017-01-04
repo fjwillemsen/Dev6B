c166df2633dc9e5be1defcbbc3655933ae29f054
mindecheng <mindecheng@live.nl>
2017-01-04 23:52:21 +0100

    VS crashed

--- ProjectTask.py
+++ ProjectTask.py
@@ -1,12 +1,13 @@
 from datetime import datetime
+from random import randint
 class ProjectTask:
     def __init__(self):
-        self.project1 = [("1-1",'task1',5),("1-2",'task2',10),("1-3",'task3',10),("1-4",'task4',10),("1-5",'task5',10),("1-6",'task6',10)];
-        self.project2 = [("2-1",'task1',5),("2-2",'task2',10),("2-3",'task3',10),("2-4",'task4',10),("2-5",'task5',10),("2-6",'task6',10)];
-        self.project3 = [("3-1",'task1',5),("3-2",'task2',10),("3-3",'task3',10),("3-4",'task4',10),("3-5",'task5',10),("3-6",'task6',10)];
-        self.project4 = [("4-1",'task1',5),("4-2",'task2',10),("4-3",'task3',10),("4-4",'task4',10),("4-5",'task5',10),("4-6",'task6',10)];
-        self.project56 = [("56-1",'task1',5),("56-2",'task2',10),("56-3",'task3',10),("56-4",'task4',10),("56-5",'task5',10),("56-6",'task6',10)];
-        self.project78 = [("78-1",'task1',5),("78-2",'task2',10),("78-3",'task3',10),("78-4",'task4',10),("78-5",'task5',10),("78-6",'task6',10)];
+        self.project1 = [["1-1",'task1',5],["1-2",'task2',10],["1-3",'task3',10],["1-4",'task4',10],["1-5",'task5',10],["1-6",'task6',10]];
+        self.project2 = [["2-1",'task1',5],["2-2",'task2',10],["2-3",'task3',10],["2-4",'task4',10],["2-5",'task5',10],["2-6",'task6',10]];
+        self.project3 = [["3-1",'task1',5],["3-2",'task2',10],["3-3",'task3',10],["3-4",'task4',10],["3-5",'task5',10],["3-6",'task6',10]];
+        self.project4 = [["4-1",'task1',5],["4-2",'task2',10],["4-3",'task3',10],["4-4",'task4',10],["4-5",'task5',10],["4-6",'task6',10]];
+        self.project56 = [["56-1",'task1',5],["56-2",'task2',10],["56-3",'task3',10],["56-4",'task4',10],["56-5",'task5',10],["56-6",'task6',10]];
+        self.project78 = [["78-1",'task1',5],["78-2",'task2',10],["78-3",'task3',10],["78-4",'task4',10],["78-5",'task5',10],["78-6",'task6',10]];
         self.listofProjectNames =["project 1","project 2","project 3","project 4","project 56","project 78"];
         self.listOfProjectTask = (self.project1,self.project2,self.project3,self.project4,self.project56,self.project78);
         self.lastActionDate = None;
@@ -33,6 +34,12 @@ class ProjectTask:
     def getProjectName(self,project_id):
         return self.listofProjectNames[project_id-1]
 
+    def addRandomTimeToTask(self,project):
+            for task in project:
+                x = randint(0,9)
+                task[2] = task[2] + x
+
+
     def updateListOftask(self,task_id):
         for projectTask in self.listOfProjectTask:
             for task in projectTask:
@@ -40,6 +47,9 @@ class ProjectTask:
                     self.cooldownSecOver = task[2]
                     self.lastActionDate = datetime.now()
                     projectTask.remove(task);
+                    self.addRandomTimeToTask(projectTask)
+
+    
 
     def isCooldownOver(self,task_id):
         if (self.lastActionDate is None):
