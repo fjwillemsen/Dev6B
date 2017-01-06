1dd6d82c6e88b99b0688d78d74856b9671079ef9
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2017-01-06 23:25:37 +0100

    Fixed the wallpaper. Users will now have to wait some time relative to the number of correct answers they gave at Development.

--- Development.py
+++ Development.py
@@ -86,8 +86,8 @@ class Development:
             db.setDev(newscore)
 
         if len(correct) < 1:
-            return temp.substitute(text = "You didn't pass a single test. You failed us.")
+            return temp.substitute(text = "You didn't pass a single test. You failed us.", delay = 360)
         if len(correct) < 3:
-            return temp.substitute(text = "Looks like you didn't pass all tests. These are the ones you did pass: " + ", ".join(map(str, correct)))
+            return temp.substitute(text = "Looks like you didn't pass all tests. These are the ones you did pass: " + ", ".join(map(str, correct)), delay = 120)
         if len(correct) > 2:
-            return temp.substitute(text = "Congratulations, you passed every test!")
+            return temp.substitute(text = "Congratulations, you passed every test!", delay = 0)
